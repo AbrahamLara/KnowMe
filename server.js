08a8faa40385b2d5a8f2d35b8c3bbd67a9402f9b
Abraham Lara Granados <abrahamlara19@gmail.com>
2019-04-09 23:33:44 -0400

    chore: Added cors to allow only react server to make requests

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
