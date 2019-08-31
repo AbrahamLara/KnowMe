224aa4b94ab50095540a1d64a5f9706cadb399c7
AbrahamLara <abrahamlara19@gmail.com>
2019-08-31 15:19:15 -0400

    style/refactor: Updated profile page design for user image and banner

--- client/src/views/KnowPage.js
+++ client/src/views/KnowPage.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import {
-  Container,
-  Jumbotron,
-  Row
+  Container
 } from 'reactstrap';
-import default_pic from '../images/test_profile_pic.png';
+import logo from '../logo.svg';
 import '../css/KnowPage.css';
 import PropTypes from'prop-types';
 
@@ -14,21 +12,20 @@ class KnowPage extends Component {
   }
 
   render() {
-    const match = this.props.match;
+    const userLink = this.props.match.params.userLink;
 
     return (
-      <Container className='KnowPage pt-6'>
-        <Jumbotron className='bg-white' fluid>
-          <Container fluid>
-            <Row className='align-items-center mb-3'>
-              <img className='kp-user-profile-image mb-10' src={default_pic} alt='png file' />
-              <h1 className='ml-4'>{match.params.name.split('-').join(' ')}</h1>
-            </Row>
-            <div className='kp-user-description'>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-            </div>
-          </Container>
-        </Jumbotron>
+      <Container className='KnowPage pb-3'>
+        <div className="kp-user-banner">
+          <div className="kp-profile-image-container">
+            <img
+              id="profileImage"
+              src={logo}
+              alt="profile-pic"
+              draggable="false"
+            />
+          </div>
+        </div>
       </Container>
     ); 
   }
