b69c5d8d0b7ad11c122fc3032651dba086a6b6aa
AbrahamLara <abrahamlara19@gmail.com>
2019-08-31 16:45:29 -0400

    chore: Added section for user summary

--- client/src/views/KnowPage.js
+++ client/src/views/KnowPage.js
@@ -16,14 +16,20 @@ class KnowPage extends Component {
 
     return (
       <Container className='KnowPage pb-3'>
-        <div className="kp-user-banner">
-          <div className="kp-profile-image-container">
+        <div className='kp-user-banner'>
+          <div className='kp-profile-image-container'>
             <img
-              id="profileImage"
+              id='profileImage'
               src={logo}
-              alt="profile-pic"
-              draggable="false"
+              alt='profile-pic'
+              draggable='false'
             />
+          </div>
+        </div>
+        <div className='kp-user-summary border-top-0'>
+          <span id='userName'>Lorem Ipsum</span>
+          <div id='userSummary'>
+            {`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`}
           </div>
         </div>
       </Container>
